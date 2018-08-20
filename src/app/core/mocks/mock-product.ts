872a1460b87ab080d127882a4946a09e0f5850be
gpatino <gustavo.patino@bit.perceptio.net>
2018-08-20 16:53:07 -0500

    add image to mock

--- src/app/core/mocks/mock-product.ts
+++ src/app/core/mocks/mock-product.ts
@@ -7,7 +7,7 @@ export const PRODUCTS: Product[] = [
         desc: "Camisa Blanca",
         price: "60.000",
         category : "shirts",
-        img: "https://cdn.shopify.com/s/files/1/2397/7417/products/emf37csa021_blanco_frente_1024x1024.jpg"
+        img: "assets/img/shirt/1.jpg"
     }
     ,
     {
@@ -15,7 +15,7 @@ export const PRODUCTS: Product[] = [
         desc: "Pantalones carguero verde militar",
         price: "81.000",
         category : "pants",
-        img: "https://i.pinimg.com/originals/99/1e/53/991e5308edd6ae49a5fcefa0442e7d0d.jpg"
+        img: "assets/img/pant/1.jpg"
     }
     ,
     {
@@ -23,7 +23,7 @@ export const PRODUCTS: Product[] = [
         desc: "Pantalones gris drill",
         price: "70.000",
         category : "pants",
-        img: "https://oldnavy.gap.com/webcontent/0014/915/206/cn14915206.jpg"
+        img: "assets/img/pant/2.jpg"
      }
      ,
     {
@@ -31,7 +31,7 @@ export const PRODUCTS: Product[] = [
         desc: "Pantalones negro silueta slim",
         price: "76.000",
         category : "pants",
-        img: "https://images.express.com/is/image/expressfashion/0035_04769232_1378_f01?cache=on&wid=960&fmt=jpeg&qlt=85,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+        img: "assets/img/pant/3.jpg"
      }
      ,
     {
@@ -39,7 +39,7 @@ export const PRODUCTS: Product[] = [
         desc: "Pantalon carguero beige claro",
         price: "81.000",
         category : "pants",
-        img: "https://n4.sdlcdn.com/imgs/f/9/z/Verticals-cargo-pants-for-men-SDL730622778-1-cffcd.jpg"
+        img: "assets/img/pant/4.jpg"
      }
      ,
      {
@@ -47,7 +47,7 @@ export const PRODUCTS: Product[] = [
          desc: "Camisa cuadros amarilla",
          price: "83.000",
          category : "shirts",
-         img: "https://d3d71ba2asa5oz.cloudfront.net/12031895/images/q04a17t08-2.jpg"
+         img: "assets/img/shirt/2.jpg"
      }
      ,
      {
@@ -55,7 +55,7 @@ export const PRODUCTS: Product[] = [
          desc: "Camisa Azul",
          price: "70.000",
          category : "shirts",
-         img: "https://www.hawesandcurtis.co.uk/images/products/mens-curtis-blue-twill-slim-fit-shirt-high-collar-single-cuff1-NYPIA221-B01-119887-800px-1040px.jpg"
+         img: "assets/img/shirt/3.jpg"
      }
      ,
      {
@@ -63,7 +63,7 @@ export const PRODUCTS: Product[] = [
          desc: "Camisa gris en algodón",
          price: "95.000",
          category : "shirts",
-         img: "https://media.nlyman.com/i/nlyscandinavia/500225-7774_01?$productpage_slider_2x$"
+         img: "assets/img/shirt/4.jpg"
      }
      ,
      {
@@ -71,7 +71,7 @@ export const PRODUCTS: Product[] = [
          desc: "Camisa negra estampada",
          price: "98.000",
          category : "shirts",
-         img: "https://ae01.alicdn.com/kf/HTB1ITlHaHZnBKNjSZFGq6zt3FXaa/Swallow-Print-Shirt-Men-Prom-Party-Club-Fashion-Designer-Shirt-Camisa-Fashion-Masculina-Slim-Fit-Herren.jpg"
+         img: "assets/img/shirt/5.jpg"
      }
      ,
      {
@@ -79,7 +79,7 @@ export const PRODUCTS: Product[] = [
          desc: "Chaqueta casual gris plomo",
          price: "120.000",
          category : "jackets",
-         img: "https://images-cdn.azureedge.net/azure/in-resources/9d695974-9771-4356-8ec8-ed1c5dc8f290/Images/ProductImages/Source/VNGJ-901A%20revi%20(1).jpg;width=900"
+         img: "assets/img/jacket/1.jpg"
      }
      ,
      {
@@ -87,7 +87,7 @@ export const PRODUCTS: Product[] = [
         desc: "Chaqueta cuero camel",
         price: "140.000",
         category : "jackets",
-        img: "https://brandslock.com/402-thickbox_default/men-s-tan-leather-jacket-chicago.jpg"
+        img: "assets/img/jacket/2.jpg"
     }
     ,
      {
@@ -95,7 +95,7 @@ export const PRODUCTS: Product[] = [
         desc: "Chaqueta cuero negra con detalles beige",
         price: "165.000",
         category : "jackets",
-        img: "http://www.besdealshop.in/images/cat/jackets-for-men-sale2.jpg"
+        img: "assets/img/jacket/3.jpg"
     }
     ,
      {
@@ -103,7 +103,7 @@ export const PRODUCTS: Product[] = [
         desc: "Chaqueta acolchada negra",
         price: "215.000",
         category : "jackets",
-        img: "https://www.dhresource.com/0x0s/f2-albu-g7-M01-8C-6B-rBVaSlqhNrmAWjSyAAVzGsnsMls258.jpg/men-autumn-winter-leather-clothing-motorcycle.jpg"
+        img: "assets/img/jacket/4.jpg"
     }
     ,
      {
@@ -111,7 +111,7 @@ export const PRODUCTS: Product[] = [
         desc: "Chaqueta casual beige",
         price: "115.000",
         category : "jackets",
-        img: "https://ae01.alicdn.com/kf/HTB1EOrBLpXXXXcTXVXXq6xXFXXXI/Brand-New-Fashion-Style-Men-Stand-Collar-Autumn-Winter-Jackets-Casual-Wear-Male-Coats-Warm-Slim.jpg"
+        img: "assets/img/jacket/5.jpg"
     }
 
 ]   
